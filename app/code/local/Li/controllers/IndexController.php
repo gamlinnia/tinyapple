abc766b4b75280d0cfebcf98927a21f3651be443
gamlinnia <gamlinnia@hotmail.com>
2016-04-23 23:08:20 +0800

    module Li_Service

--- app/code/local/Li/controllers/IndexController.php
+++ app/code/local/Li/controllers/IndexController.php
@@ -3,7 +3,7 @@
 class Li_Service_IndexController extends Mage_Core_Controller_Front_Action
 {
 
-    public function indexAction {
+    public function indexAction () {
         echo '123';
     }
 
