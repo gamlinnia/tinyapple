00358bc08149fb192ad315a2a5c9c077f7237161
gamlinnia <gamlinnia@hotmail.com>
2016-04-24 21:20:30 +0800

    breadcrumbs of module Li_About

--- app/code/local/Li/About/Block/Content.php
+++ app/code/local/Li/About/Block/Content.php
@@ -3,4 +3,12 @@
 class Li_About_Block_Content extends Mage_Core_Block_Template
 {
 
+    public  function _prepareLayout ()
+    {
+        if ($breadcrumbs = $this->getLayout()->getBlock('breadcrumbs')) {
+            $breadcrumbs->addCrumb('home', array('label'=>$helper->__('Home'), 'title'=>$helper->__('Go to Home Page'), 'link'=>Mage::getBaseUrl()));
+            $breadcrumbs->addCrumb('product_list', array('label'=>$helper->__('About us'), 'title'=>$helper->__('About us')));
+        }
+    }
+
 }
