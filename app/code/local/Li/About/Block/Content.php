862e166bd433c0f4a1ed2cc80a3c71012e1f031f
gamlinnia <gamlinnia@hotmail.com>
2016-04-24 21:35:36 +0800

    add font awesome icon to breadcrumb

--- app/code/local/Li/About/Block/Content.php
+++ app/code/local/Li/About/Block/Content.php
@@ -7,7 +7,7 @@ class Li_About_Block_Content extends Mage_Core_Block_Template
     {
         $helper = Mage::helper('about');
         if ($breadcrumbs = $this->getLayout()->getBlock('breadcrumbs')) {
-            $breadcrumbs->addCrumb('home', array('label'=>$helper->__('Home'), 'title'=>$helper->__('Go to Home Page'), 'link'=>Mage::getBaseUrl()));
+            $breadcrumbs->addCrumb('home', array('label'=> '<i class="fa fa-home" aria-hidden="true"></i>' . $helper->__('Home'), 'title'=>$helper->__('Go to Home Page'), 'link'=>Mage::getBaseUrl()));
             $breadcrumbs->addCrumb('product_list', array('label'=>$helper->__('About us'), 'title'=>$helper->__('About us')));
         }
     }
