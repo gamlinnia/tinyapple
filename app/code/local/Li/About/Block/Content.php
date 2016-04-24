0a4311e72606eb6be8ce374eb1709d6bb933aad4
gamlinnia <gamlinnia@hotmail.com>
2016-04-24 12:02:33 +0800

    module Li_About

---
+++ app/code/local/Li/About/Block/Content.php
@@ -0,0 +1,6 @@
+<?php
+
+class Li_About_Block_Content extends Mage_Core_Block_Template
+{
+
+}
---
+++ app/code/local/Li/About/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Li_About_IndexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction () {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
