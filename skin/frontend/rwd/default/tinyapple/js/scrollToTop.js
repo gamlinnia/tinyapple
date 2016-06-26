4bc17e6c33fe56838393c6af99d7fff05408a35a
gamlinnia <intppp01>
2016-06-26 12:47:49 +0800

    init

---
+++ skin/frontend/rwd/default/tinyapple/js/scrollToTop.js
@@ -0,0 +1,33 @@
+
+jQuery(window).scroll(function (event) {
+
+    var threshold = jQuery(document).height() - jQuery(window).height() - jQuery('footer').height();
+    if (jQuery(window).scrollTop() > 0) {
+        jQuery('div.pagetopbtn img').css({
+            opacity: 1
+        });
+        if ( jQuery(window).scrollTop() >= threshold ) {
+            jQuery('#PageTopBtn').css({
+                top: "-46px",
+                right: "20px",
+                bottom: "auto",
+                position: "absolute"
+            });
+        } else {
+            jQuery('#PageTopBtn').css({
+                top: "auto",
+                right: "20px",
+                bottom: "-9px",
+                position: "fixed"
+            });
+        }
+} else {
+    jQuery('div.pagetopbtn img').css({
+        opacity: 0
+    });
+}
+});
+
+jQuery('div.pagetopbtn').click(function () {
+    jQuery("html, body").animate({scrollTop:0}, 500, 'swing');
+});
